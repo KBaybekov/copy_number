989c8e5822b13ff699b4c829b7bfd368ac55a451
KBaybekov <91565993+KBaybekov@users.noreply.github.com>
2026-03-30 15:17:40 +0300

    1517

--- src/helpers/prefect_create_variable.py
+++ src/helpers/prefect_create_variable.py
@@ -1,5 +1,5 @@
 text = """params {
-    fastq = {{ fq_dir }}
+    fastq = "{{ fq_dir }}"
     references = "/mnt/cephfs8_rw/nanopore2/service/reference/human/hg38/no_alt"
     reference_mmi_file = "/mnt/cephfs8_rw/nanopore2/service/reference/human/hg38/no_alt/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.mmi"
     out_dir = "{{ bam_out_dir }}"
@@ -39,7 +39,7 @@ workDir = "{{ sample_work_dir }}"
 print(text.replace('\n', '\\n').replace('"', '\\"'))
 
 """
-params {\n    fastq = {{ fq_dir }}\n    references = \"/mnt/cephfs8_rw/nanopore2/service/reference/human/hg38/no_alt\"\n    reference_mmi_file = \"/mnt/cephfs8_rw/nanopore2/service/reference/human/hg38/no_alt/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.mmi\"\n    out_dir = \"{{ bam_out_dir }}\"\n    prefix = \"{{ prefix }}\"\n    per_read_stats = true\n    depth_coverage = true\n    threads = {{ threads_per_alignment }}\n}\n\nprocess {\n    withName: \"pipeline:process_references:combine\" {\n        memory = 8.GB\n    }\n\nreport {\n    enabled = true\n    file = \"{{ report_file }}\"\n    overwrite = true\n    }\n\ntrace {\n    enabled = true\n    file = \"{{ trace_file }}\"\n    overwrite = true\n    }\n\ntimeline {\n    enabled = true\n    file = \"{{ timeline_file }}\"\n    overwrite = true\n    }\n}\n\nworkDir = \"{{ sample_work_dir }}\"
+params {\n    fastq = \"{{ fq_dir }}\"\n    references = \"/mnt/cephfs8_rw/nanopore2/service/reference/human/hg38/no_alt\"\n    reference_mmi_file = \"/mnt/cephfs8_rw/nanopore2/service/reference/human/hg38/no_alt/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.mmi\"\n    out_dir = \"{{ bam_out_dir }}\"\n    prefix = \"{{ prefix }}\"\n    per_read_stats = true\n    depth_coverage = true\n    threads = {{ threads_per_alignment }}\n}\n\nprocess {\n    withName: \"pipeline:process_references:combine\" {\n        memory = 8.GB\n    }\n\nreport {\n    enabled = true\n    file = \"{{ report_file }}\"\n    overwrite = true\n    }\n\ntrace {\n    enabled = true\n    file = \"{{ trace_file }}\"\n    overwrite = true\n    }\n\ntimeline {\n    enabled = true\n    file = \"{{ timeline_file }}\"\n    overwrite = true\n    }\n}\n\nworkDir = \"{{ sample_work_dir }}\"\n
 """
 
 "/mnt/cephfs8_ro/nanopore/DNA/RONC/770131741501/20250217_1356_P2S-02571-B_PAW75802_92ee5798/fastq_pass"
